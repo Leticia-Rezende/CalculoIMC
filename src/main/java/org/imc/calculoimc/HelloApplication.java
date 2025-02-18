1b1ad40d7ef652737814638db4e6021b79ee1e39
Leticia Rezende <leticiarezende218@gmail.com>
2025-02-18 19:20:28 -0300

    Calcular o IMC - Inicio

--- src/main/java/org/imc/calculoimc/HelloApplication.java
+++ src/main/java/org/imc/calculoimc/HelloApplication.java
@@ -2,15 +2,19 @@ package org.imc.calculoimc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.imc.calculoimc.utils.PathFXML;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(new FileInputStream(PathFXML.pathBase() + ))
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
